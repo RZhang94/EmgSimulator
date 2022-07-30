c8e950e42160ba2d8dc01112eb9562766b5618fa
Ray Zhang <35127627+RZhang94@users.noreply.github.com>
2022-07-30 16:39:49 -0600

    rays branch

--- emgMath.py
+++ emgMath.py
@@ -1,11 +1,15 @@
 import numpy as np
 
+def getMovingAverage(signal, samples = 5):
+    rectifiedData = rectification(signal)
+    movingAvgData = movingAverage(rectifiedData, samples = samples)
+    return movingAvgData
 def rectification(signal):
     squaredSignal = np.power(signal, 2)
     positiveSignal = np.power(squaredSignal , 1/2)
     return positiveSignal
 
-def movingAverage(signal, samples = 5):
+def movingAverage(signal, samples = 12):
     signalShape = signal.shape
     newShape = (signal.shape[0], signal.shape[1]+samples -1)
     newVector = np.zeros(shape=newShape)
