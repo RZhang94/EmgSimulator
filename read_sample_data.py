e098086357091c910fb6ce812bfc9a489b9aa387
Zhaoxiang Zhang <zhaoxia2@ualberta.ca>
2022-07-30 16:08:26 -0600

    Read the sampled data
    load and plot the data

---
+++ read_sample_data.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+# load the data
+# change your own path here
+file_path = '/Users/zhangzhaoxiang/Documents/GitHub/EmgSimulator/Ray_Sample_data/Pull_fast_1.npz'
+sample_data = np.load(file_path, allow_pickle=True)
+sample_data = sample_data['arr_0']
+
+# plot the data
+import matplotlib.pyplot as plt
+plt.plot(sample_data[100:,:])       # because the front 100 data is quite noisy/abnormal
+plt.show()
