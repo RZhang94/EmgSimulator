31d632c791e0e52548e5fd03680ef7e894d9a7fb
Zhaoxiang Zhang <zhaoxia2@ualberta.ca>
2022-07-31 19:19:36 -0600

    arm movement on both direction

---
+++ Experiment_pipeline/simpleDynamics.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('/Users/zhangzhaoxiang/Documents/GitHub/EmgSimulator')
+import emgMath
+import matplotlib.pyplot as plt
+import numpy as np
+
+gaussian = emgMath.generateGaussianCurve()
+accel, velocity, position = emgMath.integrateAccelTimeline(inputForce = gaussian, gravity= -3.95, positionLimit= 0)
+
+fig, ax = plt.subplots(3,1)
+ax[0].plot(accel)
+ax[0].set_title('acceleration')
+ax[1].plot(velocity)
+ax[1].set_title('velocity')
+ax[2].plot(position)
+ax[2].set_title('position')
+plt.show()
+
+print('hi')
