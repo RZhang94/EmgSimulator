e1d3c264510c30ab3e791c59d2e8a4fc7528cd46
Zhaoxiang Zhang <zhaoxia2@ualberta.ca>
2022-07-31 19:25:46 -0600

    nothing

--- TestCases/read_sample_data2.py
+++ TestCases/read_sample_data2.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/Users/zhangzhaoxiang/Documents/GitHub/EmgSimulator')
+
 import numpy as np
 import emgMath
 import plotUtils
@@ -6,8 +9,12 @@ sampleN = 60
 ch2Offset = 0
 trimN = 100
 
-filePath1 = r'C:\Users\joyce\OneDrive\Documents\GitHub\EmgSimulator\Ray_Sample_data\Pull_fast_1.npz'
-filePath2 = r'C:\Users\joyce\OneDrive\Documents\GitHub\EmgSimulator\Ray_Sample_data\Push_fast_1.npz'
+name = 'Ray'
+filePath1 = '/Users/zhangzhaoxiang/Documents/GitHub/EmgSimulator/{}_Sample_data/Pull_fast_1.npz'.format(name)
+filePath2 = '/Users/zhangzhaoxiang/Documents/GitHub/EmgSimulator/{}_Sample_data/Push_fast_1.npz'.format(name)
+
+# filePath1 = r'C:\Users\joyce\OneDrive\Documents\GitHub\EmgSimulator\Ray_Sample_data\Pull_fast_1.npz'
+# filePath2 = r'C:\Users\joyce\OneDrive\Documents\GitHub\EmgSimulator\Ray_Sample_data\Push_fast_1.npz'
 
 data1 = np.load(filePath1, allow_pickle=True)
 data2 = np.load(filePath2, allow_pickle=True)
